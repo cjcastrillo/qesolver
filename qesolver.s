59ff123950b5a52955db4f7a77ff2f45c556c4e3
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-12-02 00:33:57 -0800

    Added .data segment

--- qesolver.s
+++ qesolver.s
@@ -7,3 +7,31 @@
 #	Description:
 #				Use the FPU to do the qudratic formula
 #
+	
+	.data
+intro:
+	.asciiz	"Quadratic Equation Solver by C. Castrillo\n\n"
+prompta:
+	.asciiz	"Enter values for a? "
+promptb:
+	.asciiz	"Enter values for b? "
+promptc:
+	.asciiz	"Enter values for c? "
+result1:
+	.asciiz	"Not a quadratic equation."
+result2:
+	.asciiz	"Roots are imaginary."
+result3:
+	.asciiz	"x = "
+result4:
+	.asciiz "x1 = "
+result5:
+	.asciiz "x2 = "
+valuea:
+	.float	0.0
+valueb:
+	.float	0.0
+valuec:
+	.float	0.0
+
+	.text
